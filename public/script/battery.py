c9cd13623aa32f8ce267f52eb0a94b745cad3ef1
TIOGO Eric Cabrel <tericcabrel@yahoo.com>
2019-05-12 18:59:02 +0100

    Fix battery.py

--- public/script/battery.py
+++ public/script/battery.py
@@ -1,8 +1,4 @@
-Python 3.4.2 (v3.4.2:ab2c023a9432, Oct  6 2014, 22:15:05) [MSC v.1600 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> #!/usr/bin/env python
-#
-#
+#!/usr/bin/env python
 import time
 import os
 import sys
